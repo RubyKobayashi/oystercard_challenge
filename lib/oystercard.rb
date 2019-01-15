2887fcac6876474383f0a012e3081f7d8f2ef9f5
RubyKobayashi <ruby@rubykobayashi.com>
2019-01-15 14:53:56 +0000

    Passing test for raising an error when reaching maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,17 @@
 class Oystercard
+
 attr_reader :balance
+MAXIMUM_BALANCE = 90
 
   def initialize
    @balance = 0
 end
 
 def top_up(amount)
+  raise 'Maximum balance of 90 reached' if @balance + amount > MAXIMUM_BALANCE
   @balance += amount
+
+
 end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe Oystercard do
     it 'tops up the balance' do
       expect {subject.top_up(1)}.to change {subject.balance}.by(1)
     end
+
+    it 'raises an error when maximum balance is reached' do
+      subject.top_up(90)
+      expect {subject.top_up(1)}.to raise_error "Maximum balance of 90 reached"
+    end
+
    end
 end
