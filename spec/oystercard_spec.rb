3a9cfc54c118041dda49c04bfb7cbc45105e0a77
RubyKobayashi <ruby@rubykobayashi.com>
2019-01-15 14:41:39 +0000

    Passing test for initializing balance and top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,12 @@
-RSpec.describe Oystercard do
+require 'oystercard'
 
+RSpec.describe Oystercard do
+  it { is_expected.to respond_to(:balance) }
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  describe '#top-up' do
+    it 'tops up the balance' do
+      expect {subject.top_up(1)}.to change {subject.balance}.by(1)
+    end
+   end
 end
