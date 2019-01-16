c8db9fa596bae12aae34613b8a728277358c7d82
RubyKobayashi <ruby@rubykobayashi.com>
2019-01-16 15:42:42 +0000

    Step 13: passing station zone and name tests

---
+++ spec/station_spec.rb
@@ -0,0 +1,22 @@
+require 'station'
+
+RSpec.describe Station do
+  subject {described_class.new("Aldgate East", 2 )}
+
+  describe 'name' do
+    it "is expected to return the name of the station" do
+      expect(subject.name).to eq "Aldgate East"
+    end
+  end
+  describe 'zone' do
+    it "is expected to return the zone of the station" do
+      expect(subject.zone).to eq 2
+    end
+  end
+
+
+  # it {is_expected.to respond_to(:name)}
+  # it {is_expected.to respond_to(:zone)}
+
+
+end
